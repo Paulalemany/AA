81ce3383c2ab89ba9faa226351e1540f2e3a903a
Cynthia Tristán <cyntrist@ucm.es>
2025-10-09 21:52:21 +0200

    a

---
+++ Practica1/Enunciado/LinearRegression.py
@@ -0,0 +1,123 @@
+import numpy as np
+import copy
+import math
+from utils import *
+
+class LinearReg:
+    """
+    Computes the cost function for linear regression.
+
+    Args:
+        x (ndarray): Shape (m,) Input to the model
+        y (ndarray): Shape (m,) the real values of the prediction
+        w, b (scalar): Parameters of the model
+    """
+    def __init__(self, x, y,w,b):
+        self.x = x      #   Datos de referencia (Los datos reales) 'score'
+        self.y = y      #   Datos que queremos predecir 'user Score'
+        self.w = w      #   Pendiente de la recta
+        self.b = b      #   Bias de la función, necesario para que el error sea mínimo
+        
+
+    """
+    Computes the linear regression function.
+
+    Args:
+        x (ndarray): Shape (m,) Input to the model
+    
+    Returns:
+        the linear regression value
+    """
+    # Calcula la y_prima del modelo (Los datos que estamos adivinando)
+    def f_w_b(self, x):
+        
+        mul = np.multiply(self.w, x)
+        return mul + self.b
+
+
+    """
+    Computes the cost function for linear regression.
+
+    Returns
+        total_cost (float): The cost of using w,b as the parameters for linear regression
+               to fit the data points in x and y
+    """
+    # Analiza como de bien se ajusta el modelo a los datos del entrenamiento,
+    # representa la desviación del modelo con respecto a los datos reales
+    def compute_cost(self):
+
+        y_prima = self.f_w_b(self.x)
+        cost = np.sum(np.square(self.y - y_prima))/(np.size(self.y) * 2)
+        #                   m                             y      y'
+        #return (1 / 2 * np.size(self.y)) * np.sum(np.square(self.y - y_prima))
+        return  cost
+    
+
+    """
+    Computes the gradient for linear regression 
+    Args:
+
+    Returns
+      dj_dw (scalar): The gradient of the cost w.r.t. the parameters w
+      dj_db (scalar): The gradient of the cost w.r.t. the parameter b     
+     """
+    # El gradiente es la derivada parcial de los datos
+    # Se utiliza en el descenso de gradiente para ver la derivada de la función en un punto
+    def compute_gradient(self):
+
+        y_prima = self.f_w_b(self.x)
+                #           m
+        dj_dw =  ((1/ np.size(self.y)) * (np.sum((y_prima - self.y) * self.x)))
+        dj_db = ((1/ np.size(self.y)) * (np.sum(y_prima - self.y)))
+        return dj_dw, dj_db
+    
+    
+    """
+    Performs batch gradient descent to learn theta. Updates theta by taking 
+    num_iters gradient steps with learning rate alpha
+
+    Args:
+      alpha : (float) Learning rate
+      num_iters : (int) number of iterations to run gradient descent
+    Returns
+      w : (ndarray): Shape (1,) Updated values of parameters of the model after
+          running gradient descent
+      b : (scalar) Updated value of parameter of the model after
+          running gradient descent
+      J_history : (ndarray): Shape (num_iters,) J at each iteration,
+          primarily for graphing later
+      w_initial : (ndarray): Shape (1,) initial w value before running gradient descent
+      b_initial : (scalar) initial b value before running gradient descent
+    """
+    # Trata de minimizar J(w,b), cambia w y b hasta conseguir el valor más cercano al mínimo posible
+    # Para ello se utiliza la función de coste
+    # Este algoritmo es el que va viendo la derivada para ver si se ha quedado corto para llegar al objetivo
+    # (Derivada decreciente) o se ha pasado (Derivada creciente) acercandose cada vez más al vértice de la función
+    # Nuestra respuesta correcta
+    def gradient_descent(self, alpha, num_iters):
+        # An array to store cost J and w's at each iteration — primarily for graphing later
+        J_history = []
+        w_history = []
+        w_initial = copy.deepcopy(self.w)  # avoid modifying global w within function
+        b_initial = copy.deepcopy(self.b)  # avoid modifying global w within function
+
+        for i in range(num_iters):
+            w,b = self.compute_gradient()
+
+            self.w = self.w - alpha * w
+            self.b = self.b - alpha * b
+
+            J_history.append(self.compute_cost())
+        
+        return self.w, self.b, J_history, w_initial, b_initial
+
+
+def cost_test_obj(x,y,w_init,b_init):
+    lr = LinearReg(x,y,w_init,b_init)
+    cost = lr.compute_cost()
+    return cost
+
+def compute_gradient_obj(x,y,w_init,b_init):
+    lr = LinearReg(x,y,w_init,b_init)
+    dw,db = lr.compute_gradient()
+    return dw,db
---
+++ Practica1/Enunciado/utils.py
@@ -0,0 +1,93 @@
+import numpy as np
+import pandas as pd
+from csv import reader
+
+
+# read the CSV file
+def load_csv(filename):
+    # # Open file in read mode
+    # file = open(filename,"r")
+    # # Reading file 
+    # lines = reader(file)
+    
+    # # Converting into a list 
+    # data = list(lines)
+
+    # return data
+    return pd.read_csv(filename)
+
+def cleanData(data):
+    data["score"] = data["score"].apply(lambda x:  str(x).replace(",","."))
+    data = data.drop(data[data["user score"] == "tbd"].index)
+    data["user score"] = data["user score"].apply(lambda x:  str(x).replace(",","."))
+    data["score"] = data["score"].astype(np.float64) / 10
+    data["user score"] = data["user score"].astype(np.float64)
+
+    return data
+
+def cleanDataMulti(data):
+    data = cleanData(data)
+    data["critics"] = data["critics"].astype(np.float64)
+    data["users"] = data["users"].astype(np.float64)
+    return data
+
+def load_data_csv(path,x_colum,y_colum):
+    data = pd.read_csv(path)
+    data = cleanData(data)
+    X = data[x_colum].to_numpy()
+    y = data[y_colum].to_numpy()
+
+    #Poner dentro de Clean data 
+    #y_ = y * 10
+    return X, y
+
+
+# Cynthia dice: holaaa he visto que esto está en
+# la diapo 13 del tema 2 reg multivariable y en wikipedia (standard score)
+def zscore_normalize_features(X):
+    """
+    computes  X, zcore normalized by column
+
+    Args:
+      X (ndarray (m,n))     : input data, m examples, n features
+
+    Returns:
+      X_norm (ndarray (m,n)): input normalized by column
+      mu (ndarray (n,))     : mean of each feature
+      sigma (ndarray (n,))  : standard deviation of each feature
+    """
+    
+    # find the mean of each column/feature
+    # mu will have shape (n,)
+    mu = np.mean(X, axis=0)
+    # find the standard deviation of each column/feature
+    # sigma will have shape (n,)
+    sigma = np.std(X, axis=0, ddof=0) 
+    # element-wise, subtract mu for that column from each example,
+    # divide by std for that column (cyntrist: <-- sigma entiendo (standard deviation))
+
+    X_norm = (X - mu) / sigma
+
+    return X_norm, mu, sigma
+
+def load_data_csv_multi(path,x1_colum,x2_colum,x3_colum,y_colum):
+    data = load_csv(path)
+    #data = pd.read_csv(path)
+    data = cleanDataMulti(data)
+    x1 = data[x1_colum].to_numpy()
+    x2 = data[x2_colum].to_numpy()
+    x3 = data[x3_colum].to_numpy()
+    X = np.array([x1, x2, x3])
+    X = X.T
+    X_norm, mu, sigma = zscore_normalize_features(X)
+    y = data[y_colum].to_numpy()
+    return X_norm, y
+
+def GetNumGradientsSuccess(w1,w1Sol,b1,b1Sol):
+    iterator = 0
+    for i in range(len(w1)): 
+        if np.isclose(w1[i],w1Sol[i]):
+                iterator += 1
+    if np.isclose(b1,b1Sol):
+        iterator += 1
+    return iterator
