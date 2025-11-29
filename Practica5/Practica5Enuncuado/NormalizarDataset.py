bb5e1963e1ca91374df2809869cda79b3b6c538d
Cynthia Tristán <cyntrist@ucm.es>
2025-11-30 00:30:57 +0100

    one hot encoding??

--- Practica5/Practica5Enuncuado/NormalizarDataset.py
+++ Practica5/Practica5Enuncuado/NormalizarDataset.py
@@ -38,15 +38,18 @@ label_column = "action"
 
 #https://www.educative.io/answers/the-fit-vs-fittransform-methods-in-scikit-learn
 # --- ONE-HOT ENCODING ---
-# ohe = OneHotEncoder(sparse_output=False)
-# ohe_data = ohe.fit_transform(df_clean[ohe_columns])
+encoder = OneHotEncoder(sparse_output=False)
+ohe_data = encoder.fit_transform(df_clean[ohe_columns])
 
-# ohe_df = pd.DataFrame(
-#     ohe_data,
-#     columns=ohe.get_feature_names_out(ohe_columns),
-#     index=df_clean.index
-# )
-# df_clean = pd.concat([df_clean.drop(columns=ohe_columns), ohe_df], axis=1)
+ohe_df = pd.DataFrame(
+    ohe_data,
+    columns=encoder.get_feature_names_out(ohe_columns), # esto hace una columna con cada posibilidad de cada atributo para que quepa todo al hacer OHE
+    index=df_clean.index
+)
+# df_clean[ohe_columns] = ohe_df esto ya no se puede hacer pq ahora hay más columnas
+df_sin_ohe = df_clean.drop(columns=ohe_columns); #quitamos los atributos orignales
+df_clean = pd.concat([df_sin_ohe, ohe_df], axis=1) #los volvemos a concatenar al final ahora que están OHE
+
 
 # --- STANDARD SCALING ---
 scaler = StandardScaler()
