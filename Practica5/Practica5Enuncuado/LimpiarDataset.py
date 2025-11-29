e10ad7d71ee08878a188c9438c05684477d8194f
Cynthia Tristán <cyntrist@ucm.es>
2025-11-29 05:19:09 +0100

    zzzzz mañana más

--- Practica5/Practica5Enuncuado/LimpiarDataset.py
+++ Practica5/Practica5Enuncuado/LimpiarDataset.py
@@ -1,25 +1,26 @@
 import pandas as pd
 import glob
+# https://docs.python.org/3/library/glob.html
 
-# debug: calculo de lineas totales para contrastar
+# todos los CSV que empiecen por "TankTraining"
 file_list = sorted(glob.glob("./Partidas Ganadas/*.csv"))
 
+# debug: calculo de lineas totales para contrastar
 total_lines = 0
 for f in file_list:
     with open(f, "r", encoding="utf-8") as file:
         lines = file.readlines()
-    total_lines += len(lines)
+        total_lines += len(lines)
 print("TOTAL DE LÍNEAS: ", total_lines)
 #las líneas esperadas son las totales menos:
 # una línea por archivo (la de win)
 # una línea por todos los archivos menos el primero (la de la cabecera)
-expected_rows = total_lines - len(file_list) - (len(file_list) - 1)
+expected_rows = total_lines - len(file_list)
+expected_rows -= (len(file_list) - 1)
 print("NUMERO ESPERADO DE LINEAS TRAS LIMPIEZA: ", expected_rows)
 
 
-# todos los CSV que empiecen por "TankTraining"
 df_list = []
-
 for i, f in enumerate(file_list):
     # leer siguiente csv
     temp_df = pd.read_csv(f)
@@ -38,8 +39,8 @@ df_clean = pd.concat(df_list, ignore_index=True)
 
 # eliminar columnas
 # MÁS LIMPIEZA AQUÍ
-# columns_to_drop = ["COMMAND_CENTER_X", "COMMAND_CENTER_Y"]
-# df_clean = df_clean.drop(columns=columns_to_drop)
+columns_to_drop = ["COMMAND_CENTER_X", "COMMAND_CENTER_Y"]
+df_clean = df_clean.drop(columns=columns_to_drop)
 
 # filtrar filas con valores incorrectos?
 #df_clean = df_clean[(df_clean["AGENT_1_X"] <= 50) & (df_clean["AGENT_2_X"] <= 50)]
