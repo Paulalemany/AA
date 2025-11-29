d510218240936b3fa339a9fb02d9254f496633af
Cynthia Tristán <cyntrist@ucm.es>
2025-11-29 05:09:34 +0100

    se me está comiendo una línea por la cara pero bueno ya está mas o menos bien

--- Practica5/Practica5Enuncuado/LimpiarDataset.py
+++ Practica5/Practica5Enuncuado/LimpiarDataset.py
@@ -1,8 +1,20 @@
 import pandas as pd
 import glob
 
-# todos los CSV que empiecen por "TankTraining"
+# debug: calculo de lineas totales para contrastar
 file_list = sorted(glob.glob("./Partidas Ganadas/*.csv"))
+
+total_lines = 0
+for f in file_list:
+    with open(f, "r", encoding="utf-8") as file:
+        lines = file.readlines()
+    total_lines += len(lines)
+print("TOTAL DE LÍNEAS: ", total_lines)
+expected_rows = total_lines - len(file_list) - (len(file_list) - 1)
+print("NUMERO ESPERADO DE LINEAS TRAS LIMPIEZA: ", expected_rows)
+
+
+# todos los CSV que empiecen por "TankTraining"
 df_list = []
 
 for i, f in enumerate(file_list):
@@ -10,8 +22,8 @@ for i, f in enumerate(file_list):
     temp_df = pd.read_csv(f)
 
     # si no es el primero, eliminar la cabecera
-    if i > 0:
-        temp_df = temp_df.iloc[1:]
+    # if i > 0: esto no hace falta por la cara????
+    #     temp_df = temp_df.iloc[1:]
 
     # eliminar "win"
     temp_df = temp_df.iloc[:-1]
@@ -32,4 +44,4 @@ df_clean = pd.concat(df_list, ignore_index=True)
 # Guardar CSV limpio
 df_clean.to_csv("./PartidasGanadas.csv", index=False)
 
-print("Dataset limpiado, número de filas: ", len(df_clean))
+print("DATASET LIMPIO, número de filas: ", len(df_clean))
